01c4394f346acf757e6246a595f8d3069e622ae1
K-Daley <kyle.daley.2@us.af.mil>
2024-07-23 14:52:04 -0400

    Type hinted most methods and variables I understood and adjusted code to be below 79 characters per line

--- raxpy/annotations/mixture.py
+++ raxpy/annotations/mixture.py
@@ -2,11 +2,10 @@ from dataclasses import dataclass
 from . import values
 
 
-
 @dataclass
 class Mixture:
     label: str
     limit: float = 1.0
 
-    def create_component_meta(self, component_label):
+    def create_component_meta(self, component_label) -> float:
         return values.Float(component_label, self.label)
--- raxpy/annotations/values.py
+++ raxpy/annotations/values.py
@@ -1,31 +1,30 @@
-from dataclasses import dataclass
 from typing import Optional, Set, Union, Tuple, List
+from dataclasses import dataclass
 
 from ..spaces import dimensions as d
 
 
-CategorySpec = Union[str, Tuple[str,str]]
+CategorySpec = Union[str, Tuple[str, str]]
 
 
 @dataclass(frozen=True, eq=True)
 class Base:
-    label:Optional[str] = None
-    tags:Optional[List[str]] = None
+    label: Optional[str] = None
+    tags: Optional[List[str]] = None
 
 
 @dataclass(frozen=True, eq=True)
 class Categorical(Base):
-    value_set:Optional[List[CategorySpec]] = None
-
+    value_set: Optional[List[CategorySpec]] = None
 
 
 @dataclass(frozen=True, eq=True)
 class Float(Base):
-    ub:Optional[float] = None
-    lb:Optional[float] = None
-    value_set:Optional[Set[float]] = None
+    ub: Optional[float] = None
+    lb: Optional[float] = None
+    value_set: Optional[Set[float]] = None
 
-    def apply_to(self, d:d.Float):
+    def apply_to(self, d: d.Float):
         d.lb = self.lb
         d.ub = self.ub
         d.value_set = self.value_set
@@ -33,12 +32,11 @@ class Float(Base):
 
 @dataclass(frozen=True, eq=True)
 class Integer(Base):
-    ub:Optional[int] = None
-    lb:Optional[int] = None
-    value_set:Optional[Set[int]] = None
+    ub: Optional[int] = None
+    lb: Optional[int] = None
+    value_set: Optional[Set[int]] = None
 
-
-    def apply_to(self, d:d.Int):
+    def apply_to(self, d: d.Int):
         d.lb = self.lb
         d.ub = self.ub
         d.value_set = self.value_set
